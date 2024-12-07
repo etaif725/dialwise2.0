1abfff3496c93d057d63a5ec2f4ceb154714cfeb
etaif725 <142711245+etaif725@users.noreply.github.com>
2024-12-06 19:18:56 -0600

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -17,7 +17,7 @@ export default function RootLayout({
         <ThemeProvider
           attribute="class"
           defaultTheme="dark"
-          enableSystem={false}
+          enableSystem={true}
           disableTransitionOnChange
         >
           <div className="relative flex min-h-screen flex-col">
