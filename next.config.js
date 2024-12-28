0322797e5cd1de17f3f019f7d8d70f2895a9e854
ETAI FISHLER <142711245+etaif725@users.noreply.github.com>
2024-12-28 01:44:50 -0600

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,22 +1,15 @@
 /** @type {import('next').NextConfig} */
 
-const withTM = require('next-transpile-modules')(['@square/web-sdk', 'react-square-web-payments-sdk']);
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const withTM = require('next-transpile-modules')(['@square/web-sdk', 'react-square-web-payments-sdk'])
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
-module.exports = withTM({
+module.exports = withTM ({
   reactStrictMode: false,
   webpack(config) {
     config.resolve.fallback = {
       ...config.resolve.fallback,
       stream: require.resolve('stream-browserify'),
     };
-    
-    // Add MiniCssExtractPlugin if needed
-    if (!config.plugins) {
-      config.plugins = [];
-    }
-    config.plugins.push(new MiniCssExtractPlugin());
-    
     config.module.rules.push({
       test: /\.css$/,
       use: [
@@ -25,11 +18,19 @@ module.exports = withTM({
         "postcss-loader",
       ],
     });
-
     return config;
   },
   images: {
     deviceSizes: [640, 750, 828, 1080, 1200, 1920, 2048, 3840],
+  },
+});
+
+const nextConfig = {
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  images: { 
+    unoptimized: false,
     remotePatterns: [
       {
         protocol: "https",
@@ -39,7 +40,6 @@ module.exports = withTM({
       },
     ],
   },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-});
+};
+
+module.exports = nextConfig;
